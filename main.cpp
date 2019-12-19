a6482692618c57387ce08a9079ef73687a492b68
MagTemTenebrius <aleksand.youtube@mail.ru>
2019-12-19 23:57:13 +0500

    lab-6

--- main.cpp
+++ main.cpp
@@ -1,12 +1,47 @@
+//
+// Created by magte on 19.12.2019.
+//
+
+#include "InfInt.h"
+#include "BigDecimal.h"
+
+
 #include <iostream>
-#include "Set.hpp"
+#include <time.h>
+//#include "wide_integer.cpp"
+
+
+using namespace std;
 
 int main() {
-    FILE* file = fopen("get.txt", "r");
 
-    Set* newset = new Set(file);
-    newset->show();
+    clock_t start, end;
+    {
+        start = clock();
+        main_InfInt();
+        InfInt num1 = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+        InfInt num2 = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+        cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+//        InfInt num3 = "1000";
+//        cout << num3 << "! = " << fact(num3) << endl;
 
-    std::cout << (newset->isField() ? "Это поле" : "Это не поле\n");
-    return 0;
+        end = clock();
+        cout << "The above code block was executed in " << ((double) end - start) / ((double) CLOCKS_PER_SEC)
+             << " second(s)" << endl << endl << endl;
+    }
+//    _test::tests();
+    {
+        start = clock();
+        BigDecimal C;
+        BigDecimal A("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+        BigDecimal B("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+        cout << A.toString() << " * " << B.toString() << " = " << (A*B).toString() << endl;
+
+        end = clock();
+
+
+        cout << "The above code block was executed in " << ((double) end - start) / ((double) CLOCKS_PER_SEC)
+             << " second(s)";
+    }
+
 }
